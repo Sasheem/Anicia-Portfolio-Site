cbc226479312652400e7b3fa6b181bf8053ffcae
Sasheem <sts12@me.com>
2021-02-22 23:11:09 -0500

    set up gatsby s3 plugin, deployed site to s3 bucket

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,26 @@
 module.exports = {
-  siteMetadata: {
-    title: "Anicia Portfolio Site",
-  },
-  plugins: [
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		title: 'Anicia Portfolio Site',
+		siteUrl: 'http://anicia-portfolio-site.s3-website-us-east-1.amazonaws.com/',
+	},
+	plugins: [
+		'gatsby-plugin-sharp',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sitemap',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+		{
+			resolve: `gatsby-plugin-s3`,
+			options: {
+				bucketName: 'anicia-portfolio-site',
+			},
+		},
+	],
 };
